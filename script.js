b0f9b4b63af646d082043d645059b6fb717b885f
Harrison Lee <laharrari@gmail.com>
2020-08-19 00:54:19 -0700

    Reformatted some components and added more functionality.

--- script.js
+++ script.js
@@ -19,11 +19,13 @@ function setTheme(mode) {
     if (mode === "light") {
         document.getElementById("theme-style").href = "default.css";
         document.getElementById("social_img").src = "./images/light_social.png"
+        document.body.style.backgroundColor = "#eaeaea";
     }
 
     if (mode === "dark") {
         document.getElementById("theme-style").href = "dark.css";
         document.getElementById("social_img").src = "./images/dark_social.jpg"
+        document.body.style.backgroundColor = "#23272A";
     }
 
     localStorage.setItem("theme", mode);
